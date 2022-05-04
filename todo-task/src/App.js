effbec7bae96efe1f15b69c0689aa9a231f1305e
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-05-04 13:55:12 +0000

    final

--- todo-task/src/App.js
+++ todo-task/src/App.js
@@ -1,67 +1,135 @@
 import { useState } from 'react';
-import Footer from './Components/Footer/Footer';
+import './App.css';
 import TodoItem from './Components/TodoItem/TodoItem';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [tempTodos, setTempTodo] = useState([]);
   const [text, setText] = useState('');
-  const [id, setId] = useState(1);
+  const [edit, setEdit] = useState('');
+  const [completed, setCompleted] = useState(false);
 
   const handleTodo = (e) => {
     e.preventDefault();
 
     const todo = {
-      id: id,
+      id: new Date(),
       text: text,
       completed: false,
     };
     setTodos([...todos, todo]);
-    setId(id + 1);
+    setTempTodo([...todos, todo]);
     setText('');
   };
 
-  const removeTodo = (id) => {
-    const new_todos = todos.filter((todo) => todo.id !== id);
-    setTodos([...new_todos]);
+  const removeTodo = (index) => {
+    const newTodoItems = [...todos];
+    newTodoItems.splice(index, 1);
+    setTodos(newTodoItems);
+    setTempTodo(newTodoItems);
   };
 
-  // const updateTodo = (todo) => {
-  //   const old_todo = todos.filter((oldTodo) => oldTodo.id !== todo.id);
-  //   old_todo[todo.id - 1] = todo;
-  //   setTodos([...old_todo]);
-  // };
+  const updateTodoItem = (e, index, text) => {
+    e.preventDefault();
+    const newTodoItems = [...todos];
+    const item = newTodoItems[index];
+    let todoObj = { text: text, completed: item.completed };
+    newTodoItems.splice(index, 1, todoObj);
+    setTodos([...newTodoItems]);
+    setTempTodo([...newTodoItems]);
+    setEdit(false);
+  };
+
+  const completeTodoItem = (index) => {
+    const newTodoItems = [...todos];
+    newTodoItems[index].completed === false
+      ? (newTodoItems[index].completed = true)
+      : (newTodoItems[index].completed = false);
+    setTodos(newTodoItems);
+    setCompleted(false);
+  };
+
+  const handleTab = (tab) => {
+    switch (tab) {
+      case 'completed':
+        const completed = todos.filter((todo) => todo.completed === true);
+        setTempTodo(completed);
+        break;
+      case 'active':
+        const activeTodos = todos.filter((todo) => todo.completed === false);
+        setTempTodo(activeTodos);
+        break;
+      case 'all':
+        setTempTodo(todos);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleClearCompleted = () => {
+    const new_todo = todos.filter((todo) => todo.completed === false);
+    setTodos(new_todo);
+    setTempTodo(new_todo);
+  };
 
   return (
     <div className='app'>
       <h1 className='header'>todos</h1>
-      <div className='todo__items'>
-        {/* input */}
-        <div className='todo__input'>
+      <div className='main'>
+        <div className='input__container'>
           <form onSubmit={handleTodo}>
             <input
               type='text'
+              placeholder='what needs to be done?'
               value={text}
               onChange={(e) => setText(e.target.value)}
-              placeholder='what needs to be done?'
+              className='main__input'
             />
           </form>
         </div>
-
-        {/* todo component */}
-        {todos.map((todo) => {
-          return (
-            <TodoItem
-              todo={todo}
-              key={todo.id}
-              removeTodo={removeTodo}
-              // updateTodo={updateTodo}
-            />
-          );
-        })}
-
-        {/* footer */}
-        <Footer />
+        <div className='todo__items'>
+          {tempTodos.map((todo, idx) => {
+            return (
+              <TodoItem
+                todo={todo}
+                key={idx}
+                removeTodo={removeTodo}
+                updateTodoItem={updateTodoItem}
+                completeTodoItem={completeTodoItem}
+                index={idx}
+                edit={edit}
+                setEdit={setEdit}
+                completed={completed}
+                setCompleted={setCompleted}
+              />
+            );
+          })}
+        </div>
       </div>
+      {todos.length > 0 && (
+        <div className='footer'>
+          <div className='task__count'>{todos.length} item(s) left</div>
+          <div className='tabs'>
+            <p className='tab' onClick={() => handleTab('all')}>
+              All
+            </p>
+            <p className='tab' onClick={() => handleTab('active')}>
+              Active
+            </p>
+            <p className='tab' onClick={() => handleTab('completed')}>
+              Completed
+            </p>
+          </div>
+          <div
+            className='clear__completed'
+            onClick={() => handleClearCompleted()}
+          >
+            {' '}
+            Clear Completed
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- todo-task/src/Components/TodoItem/TodoItem.js
+++ todo-task/src/Components/TodoItem/TodoItem.js
@@ -1,55 +1,60 @@
 import React, { useState } from 'react';
-import { FaRegCheckCircle, FaTimes } from 'react-icons/fa';
 import './TodoItem.css';
+import Delete from '../../assets/delete.svg';
+import Check from '../../assets/check.svg';
 
-function TodoItem({ todo, removeTodo }) {
-  const [edit, setEdit] = useState(false);
-  const [completed, setCompleted] = useState(false);
-  const [newText, setNewText] = useState(todo.text);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // const newTodo = {
-    //   id: todo.id,
-    //   text: newText,
-    //   completed: todo.completed,
-    // };
-    todo.text = newText;
-    setEdit(false);
-    setNewText('');
-  };
+function TodoItem({
+  todo,
+  removeTodo,
+  updateTodoItem,
+  index,
+  completeTodoItem,
+  edit,
+  setEdit,
+}) {
+  const [text, setText] = useState(todo.text);
 
   return (
-    <div className='todo' onDoubleClick={() => setEdit(true)}>
-      <div onClick={() => setCompleted(!completed)} className='complete__btn'>
-        {!edit && completed ? (
-          <FaRegCheckCircle />
-        ) : (
-          <span className='circle'></span>
-        )}
-      </div>
-
-      {edit ? (
-        <div className='edit__input'>
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <input
-              type='text'
-              value={newText}
-              onChange={(e) => setNewText(e.target.value)}
-            />
-          </form>
+    <div className='todoitem'>
+      <div className='todo__container'>
+        <span
+          className={`todo__check ${edit && 'hide'}`}
+          onClick={() => {
+            completeTodoItem(index);
+          }}
+        >
+          {todo.completed && <img src={Check} alt='check' className='check' />}
+        </span>
+        <div
+          onDoubleClick={() => setEdit(todo.id)}
+          className='todo__text__container'
+        >
+          {edit === todo.id ? (
+            <form
+              onSubmit={(e) => {
+                updateTodoItem(e, index, text);
+              }}
+            >
+              <input
+                type='text'
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                className='edit__input'
+              />
+            </form>
+          ) : (
+            <p className={`todo__text ${todo.completed && 'complete'}`}>
+              {todo.text}
+            </p>
+          )}
         </div>
-      ) : (
-        <div className='main__todo'>
-          <span className={`todo__text ${completed && 'completed'}`}>
-            {todo.text}
-          </span>
-        </div>
-      )}
-
-      <div onClick={() => removeTodo(todo.id)} className='delete__btn'>
-        <FaTimes />
       </div>
+      <img
+        src={Delete}
+        alt='delete'
+        className='delete'
+        onClick={() => removeTodo(index)}
+      />
     </div>
   );
 }
